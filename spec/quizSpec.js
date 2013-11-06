24572e0e37df9179b2602c17cb2a4a64aa548258
Dan Crews <dancrews@me.com>
2013-11-06 11:23:07 -0500

    Added last test.

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -29,7 +29,7 @@ describe('bigDiff(array)', function() {
 
 describe('averageMedian(array)', function() {
 
-  it('find the average of the center numbers in an array', function() {
+  it('return true if you can split the array into parts where the sum of one portion equals the sum of the other portion.', function() {
     expect(AVERAGE_MEDIAN([1, 2, 1, 1, 1])).toBe(true);
     expect(AVERAGE_MEDIAN([2, 1, 1, 2, 1])).toBe(false);
     expect(AVERAGE_MEDIAN([3, 4, 1, 2, 3, 1])).toBe(true);
@@ -38,11 +38,11 @@ describe('averageMedian(array)', function() {
 
 });
 
-// describe('canBalance(array)', function() {
+describe('countClumps(array)', function() {
 
-//   it('find the difference between the two largest numbers in an array', function() {
-//     expect(CAN_BALANCE([3, 6, 9, 12])).toEqual(3);
-//     expect(CAN_BALANCE([9, 20, 11, 12])).toEqual(8);
-//   });
+  it('Returns the number of times 2 or more of the same number appear sequentially', function() {
+    expect(COUNT_CLUMPS([2, 1, 1, 1, 1, 3, 3, 3, 1])).toEqual(2);
+    expect(COUNT_CLUMPS([9, 3, 3, 4, 4, 4, 6, 7, 7, 7])).toEqual(3);
+  });
 
-// });
+});
