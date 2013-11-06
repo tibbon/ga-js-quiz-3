520f9657a15953666d16090f5f8c4bbe0118820d
Emma Spencer <emma.n.spencer@gmail.com>
2013-11-06 10:53:31 -0500

    all tests written, 3/5 specs running

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -17,3 +17,40 @@ describe('a quiz', function() {
   });
 
 });
+
+describe('bigDiff', function() {
+
+  it('finds and subtracts the second largest number from the largest in an array', function() {
+      expect(bigDiff([3, 6, 9, 12])).toEqual(3);
+      expect(bigDiff([9, 20, 11, 12])).toEqual(8);
+  });
+
+});
+
+describe('averageMedian', function() {
+
+  it('returns the average of the 2 or 3 numbers in the middle of the array', function() {
+      expect(averageMedian([2, 6, 9, 11])).toEqual(7.5);
+      expect(averageMedian([9, 8, 2, 4, 7])).toEqual(14/3);
+  });
+});
+
+describe('canBalance', function() {
+
+  it('returns true if you  can split the array into parts where sum off one portion equals sum of other portion', function() {
+    expect(canBalance([1, 2, 1, 1, 1])).toBe(true);
+    expect(canBalance([2, 1, 1, 2, 1])).toBe(false);
+    expect(canBalance([3, 4, 1, 2, 3, 1])).toBe(true);
+  });
+
+});
+
+describe('countClumps', function() {
+
+  it('returns the numbers of times 2 or more of the same number appear sequentially', function() {
+    expect(countClumps([2, 1, 1, 1, 1, 3, 3, 3, 1])).toEqual(2);
+    expect(countClumps([9, 3, 3, 4, 4, 4, 6, 7, 7, 7])).toEqual(3);
+  });
+});
+
+
--- src/quiz.js
+++ src/quiz.js
@@ -12,6 +12,11 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 // bigDiff(array)
 //  [3, 6, 9, 12] => 3
 //  [9, 20, 11, 12] => 8
+var bigDiff = function(array) {
+  var new_array = array.sort(function(a,b){return a-b}),
+  i = new_array.length;
+  return new_array[i - 1] - new_array[i - 2];
+};
 
 // return the average of the 2 or 3 numbers in the middle of the array
 // (2 if the array has an even number of elements)
@@ -19,6 +24,18 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 // averageMedian(array)
 //  [2, 6, 9, 11] => 7.5
 //  [9, 8, 2, 4, 7] => 4.67
+var checkEven = function(val){
+     return (val%2 == 0);
+  };
+
+var averageMedian = function(array) {
+    var length = array.length;
+    if (checkEven(length)) {
+      return (array[length / 2 - 1] + array[(length / 2)])/2;
+    } else {
+      return (array[(length - 1) / 2 - 1] + array[((length - 1) / 2)] + array[((length - 1) / 2) + 1])/3;
+    }
+};
 
 // return true if you can split the array into parts
 // where the sum of one portion equals the sum of the other portion.
@@ -26,8 +43,25 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 //  [1, 2, 1, 1, 1] => true
 //  [2, 1, 1, 2, 1] => false
 //  [3, 4, 1, 2, 3, 1] => true
+var canBalance = function(array) {
+};
 
 // Returns the number of times 2 or more of the same number appear sequentially.
 // countClumps(array)
 //  [2, 1, 1, 1, 1, 3, 3, 3, 1] => 2
 //  [9, 3, 3, 4, 4, 4, 6, 7, 7, 7] => 3
+var countClumps = function(array) {
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
