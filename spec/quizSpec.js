9fe764d677bb7bf5826e77b4ad6770b86f378a41
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-02-21 09:37:42 -0500

    i know i'd be able to finish this, but i'm slow as haell.

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -11,9 +11,31 @@
 
 describe('a quiz', function() {
 
+  beforeEach(function() {
+    bDSet1 = new Array[3, 6, 9, 12];
+    bDSet2 = new Array[9, 20, 11, 12];
+    aMSet1 = new Array[2, 6, 9, 11];
+    aMSet2 = new Array[9, 8, 2, 4, 7];
+    cBSet1 = new Array[1, 2, 1, 1, 1];
+    cBSet2 = new Array[2, 1, 1, 2, 1];
+    cBSet3 = new Array[3, 4, 1, 2, 3, 1];
+    cCSet1 = new Array[2, 1, 1, 1, 1, 3, 3, 3, 1];
+    cCSet2 = new Array[9, 3, 3, 4, 4, 4, 6, 7, 7, 7];
+  });
+
   it('is about JavaScript and testing with Jasmine', function() {
     expect(QUIZ_TOPICS).toContain('jasmine');
     expect(QUIZ_TOPICS).toContain('javascript');
   });
+
+  it('finds the largest and second largest numbers in an array and find the difference', function(){});
+
+  it('returns the average of the middle 2 numbers in an array with an even number of elements', function(){});
+
+  it('returns the average of the middle 3 numbers in an array with an odd number of elements', function(){});
+
+  it("returns 'true' if array can be split into parts where sum of one portion is equal to the sum of the other" , function(){});
+
+  it('returns the number of times 2 or more of the same number appear sequentially', function(){});
 
 });
--- src/quiz.js
+++ src/quiz.js
@@ -6,12 +6,16 @@
 //      |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
 
 var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
+function Array([]){
+
+};
 
 // Find the largest number and second largest number
 // Then subtract the difference.
 // bigDiff(array)
 //  [3, 6, 9, 12] => 3
 //  [9, 20, 11, 12] => 8
+var bigDiff = function() {}
 
 // return the average of the 2 or 3 numbers in the middle of the array
 // (2 if the array has an even number of elements)
@@ -19,6 +23,7 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 // averageMedian(array)
 //  [2, 6, 9, 11] => 7.5
 //  [9, 8, 2, 4, 7] => 4.67
+var averageMedian = function() {}
 
 // return true if you can split the array into parts
 // where the sum of one portion equals the sum of the other portion.
@@ -26,8 +31,10 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 //  [1, 2, 1, 1, 1] => true
 //  [2, 1, 1, 2, 1] => false
 //  [3, 4, 1, 2, 3, 1] => true
+var canBalance = function() {}
 
 // Returns the number of times 2 or more of the same number appear sequentially.
 // countClumps(array)
 //  [2, 1, 1, 1, 1, 3, 3, 3, 1] => 2
 //  [9, 3, 3, 4, 4, 4, 6, 7, 7, 7] => 3
+var countClumps = function() {}
