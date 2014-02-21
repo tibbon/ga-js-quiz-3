153a58ac0480d007c88774084c9b2018cbf1da0b
Becky Bell <rlbell@alum.bu.edu>
2014-02-21 09:33:39 -0500

    jasmine quiz

--- src/quiz.js
+++ src/quiz.js
@@ -13,12 +13,31 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 //  [3, 6, 9, 12] => 3
 //  [9, 20, 11, 12] => 8
 
+var subtract_arrays = function() {
+	var array_1 = [3, 6, 9, 12],
+			sorted_array_1 = array_1.sort();
+			array_2 = [9, 20, 11, 12];
+			sorted_array_2 = array_2.sort();
+			diff_array_1 = sorted_array_1[0] - sorted_array_1[1];
+			diff_array_2 = sorted_array_2[0] - sorted_array_2[1];
+				return diff_array_1;
+				return diff_array_2;
+};
+
 // return the average of the 2 or 3 numbers in the middle of the array
 // (2 if the array has an even number of elements)
 // (3 if the array has an odd number of elements).
 // averageMedian(array)
 //  [2, 6, 9, 11] => 7.5
 //  [9, 8, 2, 4, 7] => 4.67
+
+var average_nums = function() {
+	var new_array = [];
+			if(% new_array.length / 2 === 0) {
+
+			}
+
+};
 
 // return true if you can split the array into parts
 // where the sum of one portion equals the sum of the other portion.
