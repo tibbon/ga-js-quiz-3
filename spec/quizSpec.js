fc648173e39c34dbba1968dd3b1b6ef03b78bfa1
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-21 09:08:59 -0500

    test for bigDiff

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -17,3 +17,10 @@ describe('a quiz', function() {
   });
 
 });
+
+describe('find largest', function(){
+  it ('returns the subtract of the largest and the second largest number', function(){
+    expect(bigDiff([3, 6, 9, 12])).toEqual(3);
+    expect(bigDiff([9, 20, 11, 12])).toEqual(8);
+  });
+});
