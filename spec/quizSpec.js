d42ab4537904e422e06dd44266c9d97d0388077a
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-30 15:21:13 -0400

    wrote all the tests out first

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -17,3 +17,40 @@ describe('a quiz', function() {
   });
 
 });
+
+describe('bigDiff', function() {
+
+  it('finds the difference between the smallest and largest number', function() {
+    expect(quiz.bigDiff([3, 6, 9, 12])).toEqual(3);
+    expect(quiz.bigDiff([9, 20, 11, 12])).toEqual(8);
+  });
+
+});
+
+describe('averageMedian', function() {
+
+  it('return the average of the 2 (if even) or 3 (if odd) middle numbers', function() {
+    expect(quiz.averageMedian([2, 6, 9, 11])).toEqual(7.5);
+    expect(quiz.averageMedian([9, 8, 2, 4, 7])).toEqual(4.67);
+  });
+
+});
+
+describe('canBalance', function() {
+
+  it('checks if array can split into equal parts', function() {
+    expect(quiz.canBalance([1, 2, 1, 1, 1])).toBeTruthy();
+    expect(quiz.canBalance([2, 1, 1, 2, 1])).toBeFalsy();
+    expect(quiz.canBalance([3, 4, 1, 2, 3, 1])).toBeTruthy();
+  });
+
+});
+
+describe('countClumps', function() {
+
+  it('counts the number of times 2 or more of the same number appear sequentially.', function() {
+    expect(quiz.countClumps([2, 1, 1, 1, 1, 3, 3, 3, 1])).toEqual(2);
+    expect(quiz.countClumps([9, 3, 3, 4, 4, 4, 6, 7, 7, 7])).toEqual(3);
+  });
+
+});
